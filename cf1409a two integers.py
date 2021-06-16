763824d7357e7888ca34b77651a9e3e2f138a0cc
_tailwind <46327788+tailwind01@users.noreply.github.com>
2021-06-16 22:02:25 +0530

    Add files via upload

---
+++ cf1409a two integers.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 16 16:54:46 2021
+
+@author: sai
+"""
+nc = int(input())
+for _ in range(nc):
+    a,b = map(int, input().rstrip().split())
+    absDiff = abs(a-b)
+    if absDiff==0:
+        print(0)
+    if absDiff<10:
+        print(1)
+    elif absDiff%10!=0:
+        print(absDiff//10 + 1)
+    else:
+        print(absDiff//10)
+
