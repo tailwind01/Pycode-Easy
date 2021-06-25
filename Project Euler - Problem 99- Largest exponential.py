72894d8ccf7e678a8579ad1a1c3158472d9fc4b0
_tailwind <46327788+tailwind01@users.noreply.github.com>
2021-06-25 16:18:25 +0530

    Create Project Euler - Problem 99- Largest exponential.py

---
+++ Project Euler - Problem 99- Largest exponential.py
@@ -0,0 +1,21 @@
+# ##Comparing two numbers written in index form like 211 and 37 is not difficult, as any calculator would confirm that 211 = 2048 < 37 = 2187.
+
+# However, confirming that 632382518061 > 519432525806 would be much more difficult, as both numbers contain over three million digits.
+
+# Using base_exp.txt (right click and 'Save Link/Target As...'), a 22K text file containing one thousand lines with a base/exponent pair on each line, determine which line number has the greatest numerical value.
+
+# a tailwind implementation
+import math as m
+l = 1000
+mod = pow(10,9)+7
+inpl=[]
+for i in range(l):
+    base, exponent = map(int, input().split(","))
+    #we will calculate values using logarithmic rules
+    ln_ans = exponent*m.log(base)
+    inpl+=[ln_ans]
+
+maxnum = max(inpl) #naturally, number with largest ln_ans is largest
+
+print(inpl.index(maxmodulo)+1)
+    
